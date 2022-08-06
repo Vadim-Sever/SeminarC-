85eac96c075da297e62f9a835b6a2288ede072dc
Vadim <kvn.alt@gmail.com>
2022-08-06 13:55:47 +0700

    progect2

---
+++ Exam1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Your name?");
+string username = Console.ReadLine();
+Console.Write("Hi,");
+Console.Write(username);
